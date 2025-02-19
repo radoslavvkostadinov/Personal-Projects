45d2b418bf3e9771195b4c5c04e0f476a9b8d72a
Radoslav Kostadinov <bounslay@gmail.com>
2025-02-19 13:26:02 +0200

    Tap testing and gitignore added

---
+++ SpaceX/src/tap_spacex.py
@@ -0,0 +1,29 @@
+import singer
+import pandas as pd
+
+LOGGER = singer.get_logger()
+
+schema = {
+    'properties': {
+        'id': {'type': 'string'},
+        'name': {'type': 'string'},
+        'date_utc': {'type': 'string', 'format': 'date-time'},
+        'success':{'type': 'boolean'},
+        'rocket': {'type': 'string'}
+    }
+}
+
+def main():
+    url = 'https://api.spacexdata.com/v5/launches'
+    
+    df = pd.read_json(url)
+    df = df[['id', 'name', 'date_utc', 'success', 'rocket']]
+    df['success'] = df['success'].apply(lambda x: False if x == 0.0 else True if x == 1.0 else "")
+
+    records = df.to_dict(orient='records')
+
+    singer.write_schema('launches', schema, 'id')
+    singer.write_records('launches', records)
+
+if __name__ == '__main__':
+    main()

34a474270c53169f34c0dfbf74dc89b4ddb5e660
Radoslav Kostadinov <bounslay@gmail.com>
2025-02-20 13:13:54 +0200

    Delete output.json and replace with new one called launches_output.json

--- SpaceX/src/tap_spacex.py
+++ SpaceX/src/tap_spacex.py
@@ -3,27 +3,30 @@ import pandas as pd
 
 LOGGER = singer.get_logger()
 
-schema = {
+
+
+def fetch_launches():
+    url = 'https://api.spacexdata.com/v4/launches'
+    
+    df = pd.read_json(url)
+    df = df[['id', 'name', 'date_utc', 'success', 'rocket']]
+    df['success'] = df['success'].apply(lambda x: True if x == 1.0 else False if x == 0.0 else 'No info')
+    
+    records = df.to_dict(orient='records')
+    
+    schema = {
     'properties': {
         'id': {'type': 'string'},
         'name': {'type': 'string'},
         'date_utc': {'type': 'string', 'format': 'date-time'},
-        'success':{'type': 'boolean'},
+        "success": {"type": ["number", "null"]},
         'rocket': {'type': 'string'}
     }
 }
-
-def main():
-    url = 'https://api.spacexdata.com/v5/launches'
-    
-    df = pd.read_json(url)
-    df = df[['id', 'name', 'date_utc', 'success', 'rocket']]
-    df['success'] = df['success'].apply(lambda x: True if x == 1.0 else False)
-
-    records = df.to_dict(orient='records')
-
+  
     singer.write_schema('launches', schema, 'id')
     singer.write_records('launches', records)
 
+
 if __name__ == '__main__':
-    main()
+    fetch_launches()

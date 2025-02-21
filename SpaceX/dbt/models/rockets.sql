6d64839870cfc0a5e39b3a6e29c5725c0d6a8d3f
Radoslav Kostadinov <bounslay@gmail.com>
2025-02-21 12:22:03 +0200

    Added tests for rockets.sql model and update sql_queries file

--- SpaceX/dbt/models/rockets.sql
+++ SpaceX/dbt/models/rockets.sql
@@ -1,6 +1,6 @@
 SELECT id,
 name,
-date(to_timestamp(first_flight)),
+date(to_timestamp(first_flight)) AS first_flight,
 cost_per_launch,
 country,
 success_rate_pct,
--- SpaceX/sql/sql_queries.sql
+++ SpaceX/sql/sql_queries.sql
@@ -28,3 +28,56 @@ FROM de_db.dbt_spacex.launches
 GROUP BY rocket;
 
 -------------------------------------
+
+SELECT
+    rocket,
+    AVG(
+        CASE
+            WHEN success = true THEN 1 ELSE 0 END 
+    ) AS success_rate
+FROM de_db.dbt_spacex.launches
+WHERE success IS NOT NULL
+GROUP BY rocket;
+
+-------------------------------------
+
+---- Trying to handle null values in Snowflake in proper way ----
+SELECT l.name AS launch_name,
+r.name AS rocket_name,
+r.country,
+r.first_flight,
+l.launch_date,
+r.cost_per_launch,
+r.success_rate_pct,
+r.active,
+COALESCE(TO_VARCHAR(l.success), 'Unknown') AS success_status
+FROM de_db.dbt_spacex.rockets r
+JOIN de_db.dbt_spacex.launches l 
+ON r.id = l.rocket;
+
+-------------------------------------
+
+SELECT YEAR(launch_date) AS launch_year,
+COUNT(*) AS launches_number
+FROM de_db.dbt_spacex.launches
+GROUP BY launch_year;
+
+-------------------------------------
+
+SELECT *
+FROM de_db.dbt_spacex.launches
+WHERE launch_date = (
+    SELECT MAX(launch_date)
+    FROM de_db.dbt_spacex.launches
+);
+
+-------------------------------------
+
+WITH recent_launches AS (
+    SELECT *
+    FROM de_db.dbt_spacex.launches
+    WHERE launch_date > '2020-01-01'
+)
+
+SELECT *
+FROM recent_launches;
